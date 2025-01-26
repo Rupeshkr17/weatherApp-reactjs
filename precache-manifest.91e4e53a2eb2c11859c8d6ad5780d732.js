83d51f9e6778fcdbdfc21eac94bb83e78b87f92d
Rupesh <148478525+Rupeshkr17@users.noreply.github.com>
2025-01-26 23:59:32 +0530

    Updates

---
+++ precache-manifest.91e4e53a2eb2c11859c8d6ad5780d732.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d093c1b905fe81d943f7417e7d6c9434",
+    "url": "/weatherApp-reactjs/index.html"
+  },
+  {
+    "revision": "5f24894e5fc2d11e46df",
+    "url": "/weatherApp-reactjs/static/css/main.35067b03.chunk.css"
+  },
+  {
+    "revision": "8e0ef78b07aa71303b3a",
+    "url": "/weatherApp-reactjs/static/js/2.09b79422.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weatherApp-reactjs/static/js/2.09b79422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f24894e5fc2d11e46df",
+    "url": "/weatherApp-reactjs/static/js/main.81c9aa7d.chunk.js"
+  },
+  {
+    "revision": "b31cb64811690f7b4967",
+    "url": "/weatherApp-reactjs/static/js/runtime-main.ac8de654.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weatherApp-reactjs/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/weatherApp-reactjs/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/weatherApp-reactjs/static/media/city.e1698332.jpg"
+  }
+]);
